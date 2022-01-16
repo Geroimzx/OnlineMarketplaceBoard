b38c7a00de9bd83c26102b2e5def5466082c1825
geroimzx <muhayloscherban1@gmail.com>
2022-01-16 18:30:05 +0200

    c

---
+++ src/main/java/com/geroimzx/market/OnlineMarketplaceBoardApplication.java
@@ -0,0 +1,13 @@
+package com.geroimzx.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMarketplaceBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMarketplaceBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geroimzx/market/OnlineMarketplaceBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geroimzx.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMarketplaceBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
